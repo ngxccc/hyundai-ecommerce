099fd05e1eb59c23277e8e137c40b3abcdd6a059
ngxc <ngoc.shintrann@gmail.com>
2026-05-24 22:41:32 +0700

    feat(eslint-config): restructure ESLint configuration

--- packages/eslint-config/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as base } from "./base";
-export { default as react } from "./react";
-export { default as next } from "./next";
-export { default as architecture } from "./architecture";
