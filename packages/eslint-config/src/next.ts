72a126103f48dc1ed707bd8d96bb1ca7518436bb
ngxc <ngoc.shintrann@gmail.com>
2026-05-25 23:16:09 +0700

    refactor(eslint): clean up eslint configuration and remove unused global ignores

--- packages/eslint-config/src/next.ts
+++ packages/eslint-config/src/next.ts
@@ -1,8 +1,17 @@
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
+  globalIgnores([
+    ".next/**",
+    "out/**",
+    "dist/**",
+    "build/**",
+    "next-env.d.ts",
+    "src/shared/components/ui/**",
+  ]),
+
   {
     files: ["**/*.ts", "**/*.tsx"],
     extends: [nextVitals, nextTs],
