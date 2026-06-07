7ff6d5eb7956e4440da8dbdcb85ea709a213b3b6
ngxc <ngoc.shintrann@gmail.com>
2026-06-07 16:42:04 +0700

    refactor(schema): add index to enhance preformance

--- packages/database/src/schemas/order.schema.ts
+++ packages/database/src/schemas/order.schema.ts
@@ -1,6 +1,14 @@
-import { numeric, pgEnum, snakeCase, text, uuid } from "drizzle-orm/pg-core";
+import {
+  index,
+  numeric,
+  pgEnum,
+  snakeCase,
+  text,
+  uuid,
+} from "drizzle-orm/pg-core";
 import { users } from "./auth.schema";
 import { baseEntity } from "./helpers.schema";
+import { sql } from "drizzle-orm";
 
 export const orderStatusEnum = pgEnum("order_status", [
   "pending",
@@ -11,16 +19,29 @@ export const orderStatusEnum = pgEnum("order_status", [
   "refunded",
 ]);
 
-export const orders = snakeCase.table("order", {
-  ...baseEntity,
-  userId: uuid()
-    .notNull()
-    .references(() => users.id, { onDelete: "restrict" }),
-  status: orderStatusEnum().notNull().default("pending"),
-  shippingFee: numeric({ precision: 15, scale: 2 }).notNull(),
-  shippingAddress: text().notNull(),
-  totalAmount: numeric({ precision: 15, scale: 2 }).notNull(),
-});
+export const orders = snakeCase.table(
+  "order",
+  {
+    ...baseEntity,
+    userId: uuid()
+      .notNull()
+      .references(() => users.id, { onDelete: "restrict" }),
+    status: orderStatusEnum().notNull().default("pending"),
+    shippingFee: numeric({ precision: 15, scale: 2 }).notNull(),
+    shippingAddress: text().notNull(),
+    totalAmount: numeric({ precision: 15, scale: 2 }).notNull(),
+  },
+  (table) => [
+    index("order_user_status_created_idx").on(
+      table.userId,
+      table.status,
+      table.createdAt,
+    ),
+    index("order_active_metrics_idx")
+      .on(table.createdAt)
+      .where(sql`${table.status} != 'cancelled'`),
+  ],
+);
 
 export type TOrder = typeof orders.$inferSelect;
 export type TNewOrder = typeof orders.$inferInsert;
